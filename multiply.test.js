5e6ea2ce91e8ba985b1ae12d575a7ac548683915
Guilherme Heuser Prestes <gprestes@thoughtworks.com>
2020-02-25 11:30:05 +0100

    Move test inside the describe group

--- multiply.test.js
+++ multiply.test.js
@@ -4,14 +4,14 @@ describe('test multiply positive scenarios',() => {
     test('multiply 3*2 should equal to 6', () => {
         expect(multiply(3,2)).toBe(6)
     });
+
+    test('multiply 4*3 should equal to 11', () => {
+        expect(multiply(4,3)).not.toBe(11)
+    });
 })
 
 test('object example', () => {
     const data = {first: 1};
     data['second'] = 2;
     expect(data).toEqual({first:1, second:2});
-});
-
-test('multiply 4*3 should equal to 11', () => {
-    expect(multiply(4,3)).not.toBe(11)
 });
