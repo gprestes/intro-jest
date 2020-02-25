4965a52f4c911d6745f0b8a8679437cd36533d01
Guilherme Heuser Prestes <gprestes@thoughtworks.com>
2020-02-25 14:45:52 +0100

    Add array check

--- matchersExpanded.test.js
+++ matchersExpanded.test.js
@@ -21,3 +21,14 @@ test('null', () => {
     expect(n).not.toBeTruthy()
     expect(n).toBeFalsy()
 })
+
+const carStock = [
+    'BMW',
+    'Mercedes',
+    'Ferrari',
+    'Toyota',
+];
+
+test('that the car stock list has a Ferrari', () => {
+    expect(carStock).toContain('Ferrari')
+})
